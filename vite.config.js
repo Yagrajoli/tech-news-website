d0405c5ce09819983e0fa7b466ef9cb5a38c1aa2
Yagraj <yagrajoli45@gmail.com>
2024-04-21 02:31:13 +0545

    proxy set

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+      proxy: "http://hn.algolia.com/api/v1/search?"
+  },
   plugins: [react()],
 })
